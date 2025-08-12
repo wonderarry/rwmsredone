4176b533f82afcc04338ffde95cb8a45917ecac9
carrot <triplenonick@gmail.com>
2025-08-13 00:21:53 +0400

    started implementing ports

--- internal/app/ports.go
+++ internal/app/ports.go
@@ -1,1 +1,39 @@
 package app
+
+import (
+	"context"
+
+	"github.com/wonderarry/rwmsredone/internal/domain"
+)
+
+type UnitOfWork interface {
+	WithTx(ctx context.Context, fn func(ctx context.Context, tx Tx) error) error
+}
+
+type Tx interface {
+	Accounts() AccountRepo
+	Identities() IdentityRepo
+	Projects() ProjectRepo
+	Processes() ProcessRepo
+	Approvals() ApprovalRepo
+	Outbox() OutboxRepo
+}
+
+type AccountRepo interface {
+	Get(ctx context.Context, id domain.AccountID) (*domain.Account, error)
+	Create(ctx context.Context, a *domain.Account) error
+	UpdateProfile(ctx context.Context, a *domain.Account) error
+	AddGlobalRole(ctx context.Context, a *domain.Account) error
+	HasGlobalRole(ctx context.Context, a *domain.Account) error
+}
+
+type IdentityRepo interface {
+	Create(ctx context.Context, i *domain.Identity) error
+	FindByProviderSubject(ctx context.Context, p domain.IdentityProvider, sub string) (*domain.Identity, error)
+
+	// Unsure about these currently
+
+	// FindLocalByLogin(ctx context.Context, login string) (*domain.Identity, error)
+	// GetLocalCredentials(ctx context.Context, identityID int64) (*domain.LocalCredentials, error)
+	// UpsertLocalCredentials(ctx context.Context, c domain.LocalCredentials) error
+}
